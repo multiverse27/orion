db9e40d5d916bde99f94283d2408d60978021142
Enrique Vergara <brandon.chavez@udea.edu.co>
2026-05-19 18:52:40 -0500

    Se agregan los demás rutas de los controladores

--- src/main/java/com/unal/andromeda/controllers/EmployeesController.java
+++ src/main/java/com/unal/andromeda/controllers/EmployeesController.java
@@ -1,5 +1,7 @@
 package com.unal.andromeda.controllers;
 
+import com.unal.andromeda.entities.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,62 @@ import java.util.List;
 @RestController
 public class EmployeesController {
     @RequestMapping(value="/employees")
-    public List<String> empleados(){
-        return List.of("Enrique", "Carlos", "Javier");
+    public Employee getEmployees() {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Enrique");
+        employee.setApellido("Vergara");
+        employee.setRole("Developer");
+        employee.setEnterprise("Londoño Gómez");
+        employee.setProfile("kiikechavez");
+        return employee;
+    }
+
+    @RequestMapping(value="/employees/{id}")
+    public Employee getEmployees(@PathVariable Integer id) {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Enrique");
+        employee.setApellido("Vergara");
+        employee.setRole("Developer");
+        employee.setEnterprise("Londoño Gómez");
+        employee.setProfile("kiikechavez");
+        return employee;
+    }
+
+    @RequestMapping(value="/employees")
+    public Employee modifyEmployees() {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Enrique");
+        employee.setApellido("Vergara");
+        employee.setRole("Developer");
+        employee.setEnterprise("Londoño Gómez");
+        employee.setProfile("kiikechavez");
+        return employee;
+    }
+
+    @RequestMapping(value="/employees")
+    public Employee deleteEmployees() {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Enrique");
+        employee.setApellido("Vergara");
+        employee.setRole("Developer");
+        employee.setEnterprise("Londoño Gómez");
+        employee.setProfile("kiikechavez");
+        return employee;
+    }
+
+    @RequestMapping(value="/employees")
+    public Employee searchEmployees() {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Enrique");
+        employee.setApellido("Vergara");
+        employee.setRole("Developer");
+        employee.setEnterprise("Londoño Gómez");
+        employee.setProfile("kiikechavez");
+        return employee;
     }
 }
--- src/main/java/com/unal/andromeda/entities/Employee.java
+++ src/main/java/com/unal/andromeda/entities/Employee.java
@@ -1,18 +1,19 @@
 package com.unal.andromeda.entities;
 
 public class Employee {
-    private Long id;
+    private Integer id;
     private String cedula;
     private String name;
     private String apellido;
+    private String role;
     private String enterprise;
     private String profile;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -38,6 +39,14 @@ public class Employee {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getEnterprise() {
